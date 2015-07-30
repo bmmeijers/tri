508091fce503fafbcc18c0771d96b9cea9adda56
Martijn Meijers <b.m.meijers@tudelft.nl>
2015-07-30 10:13:33 +0200

    Output also info property.

--- src/tri/delaunay.py
+++ src/tri/delaunay.py
@@ -354,17 +354,17 @@ def dest(side):
 
 def output_vertices(V, fh):
     """Output list of vertices as WKT to text file (for QGIS)"""
-    fh.write("id;wkt\n")
+    fh.write("id;wkt;info\n")
     for v in V:
-        fh.write("{0};POINT({1})\n".format(id(v), v))
+        fh.write("{0};POINT({1});{2}\n".format(id(v), v, v.info))
 
 def output_triangles(T, fh):
     """Output list of triangles as WKT to text file (for QGIS)"""
-    fh.write("id;wkt;n0;n1;n2;v0;v1;v2\n")
+    fh.write("id;wkt;n0;n1;n2;v0;v1;v2;info\n")
     for t in T:
         if t is None:
             continue
-        fh.write("{0};{1};{2[0]};{2[1]};{2[2]};{3[0]};{3[1]};{3[2]}\n".format(id(t), t, [id(n) for n in t.neighbours], [id(v) for v in t.vertices]))
+        fh.write("{0};{1};{2[0]};{2[1]};{2[2]};{3[0]};{3[1]};{3[2]};{4}\n".format(id(t), t, [id(n) for n in t.neighbours], [id(v) for v in t.vertices], t.info))
 
 def output_edges(E, fh):
     fh.write("id;side;wkt\n")
