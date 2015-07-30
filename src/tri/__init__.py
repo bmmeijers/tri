987ed7be9646ec8189392ad3b060b531e6b5e857
Martijn Meijers <b.m.meijers@tudelft.nl>
2015-07-30 10:13:51 +0200

    Preparing release 0.3

--- src/tri/__init__.py
+++ src/tri/__init__.py
@@ -1,7 +1,7 @@
 """Tri - Constrained Delaunay Triangulation of Planar Straight Line Graphs
 """
 
-__version__ = '0.3.dev0'
+__version__ = '0.3'
 __license__ = 'MIT License'
 __author__ = 'Martijn Meijers'
 
